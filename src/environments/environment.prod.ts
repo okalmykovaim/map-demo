a9bec788e0c98d227d6c58981fff6af1d9b69bb5
okalmykov <oleg.kalmykov.ua@gmail.com>
2020-09-21 19:53:28 +0300

    env fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  production: false,
+  production: true,
   googleMapsApiKey: 'AIzaSyB5TbbqY9VoiYiIBrH0W-Fw-zVW71PM2xI',
   hereApiKey: 'Qw1lOfOOh8966Dk1bxO8ahKT0zzooq48FtbQ9SA3iZ4',
   initialLatitude: 52.5347999,
