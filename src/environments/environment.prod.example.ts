0396528c77e1dcc91338f2d95a58c5dfa2574625
okalmykov <oleg.kalmykov.ua@gmail.com>
2020-09-21 18:39:23 +0300

    updated for heroku

--- src/environments/environment.prod.example.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  production: true,
-  googleMapsApiKey: 'apikey',
-  hereApiKey: 'apikey',
-  initialLatitude: 52.5347999,
-  initialLongitude: 13.3267832,
-};
---
+++ src/environments/environment.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: false,
+  googleMapsApiKey: 'AIzaSyB5TbbqY9VoiYiIBrH0W-Fw-zVW71PM2xI',
+  hereApiKey: 'Qw1lOfOOh8966Dk1bxO8ahKT0zzooq48FtbQ9SA3iZ4',
+  initialLatitude: 52.5347999,
+  initialLongitude: 13.3267832,
+};
